dcfaa3d69e14d8330deaa28d5e200b06a048ef99
bu-lo <161450788+bu-lo@users.noreply.github.com>
2024-06-03 10:02:46 +0200

    End of SuscribersInTwoYears and Creation of SumInteger

--- Niv2/SuscribersInTwoYears.java
+++ Niv2/SuscribersInTwoYears.java
@@ -3,26 +3,30 @@
  * Employer participation rate
  */
 
+public class SuscribersInTwoYears {
 
- public class SuscribersInTwoYears {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         suscribersInTwoYears();
     }
 
 // Calculating the suscribers of the Youtuber in 24 months
+    public static void suscribersInTwoYears() {
+        int subInTwoYears = 2500;
+        int nbMonth = 24;
+        int i = 0;
 
-public static void suscribersInTwoYears() {
-int subInTwoYears = 2500;
-int nbMonth = 24;
+        /*for (int i=0; i < nbMonth ; i++) {
+*subInTwoYears += subInTwoYears * 0.05 ;
+*}
+         */
+        while (i < nbMonth) {
+            subInTwoYears += subInTwoYears * 0.05;
+            i++;
+        }
+        System.out.println("Hello, if you have 2500 subscribers, and you earn + 5% per month\n");
+        System.out.println("In 24 months you should have " + subInTwoYears + " subscribers.\n");
+        System.out.println("So work hard, and GO FOR IT !\n");
+        
 
-for (int i=0; i < nbMonth ; i++) {
-    subInTwoYears += subInTwoYears * 0.05 ;
+    }
 }
-
-
-System.out.println("Hello, if you have 2500 subscribers, and you earn + 5% per month\n");
-System.out.println("In 24 months you should have " + subInTwoYears + " subscribers.\n");
-System.out.println("So work hard, and GO FOR IT !\n");
-}
-
- }
