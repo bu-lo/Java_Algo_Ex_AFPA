197948a2ac740e71f74d178baea48f25f4c968a0
bu-lo <161450788+bu-lo@users.noreply.github.com>
2024-06-03 14:21:57 +0200

    FactorialN done

---
+++ Niv2/FactorialN.java
@@ -0,0 +1,37 @@
+/*
+ * Exercise 4.5 level 2 - "Alternative control structures"
+ * Factorial N
+ */
+
+import java.util.Scanner; // Import the Scanner class 
+
+public class FactorialN {
+
+    public static void main (String[] args) {
+        userInput();
+}
+
+public static void userInput() {
+    Scanner myObj = new Scanner (System.in);
+
+    System.out.println("Choose the valor n please");
+    int n = myObj.nextInt();
+    System.out.println("----------------");
+
+    factorialN(n);
+
+    myObj.close();
+}
+
+public static void factorialN (int n) {
+    int totfacn = 1;
+
+    for (int i = 1 ; i<=n ; i++){
+        totfacn = totfacn * i;
+    }
+
+    System.out.println("The factorial of "+n+ " is " + totfacn + " .");
+
+}
+
+}
