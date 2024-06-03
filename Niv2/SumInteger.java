b5680df0db91ed1b1b6ee4f5fc8f73c6b832ae08
bu-lo <161450788+bu-lo@users.noreply.github.com>
2024-06-03 11:45:49 +0200

    End of ex SumInteger

--- Niv2/SumInteger.java
+++ Niv2/SumInteger.java
@@ -1,3 +1,37 @@
+/*
+ * Exercise 4.3 level 2 - "Alternative control structures"
+ * Integer sum of n elements
+ */
+
+import java.util.Scanner; // Import the Scanner class 
+
 public class SumInteger {
+
+    public static void main (String[] args) {
+       userInput(); 
+}
+
+public static void userInput() {
+    Scanner myObj = new Scanner(System.in);
+
+    System.out.println("Choose the valor n please");
+    int n = myObj.nextInt();
+
+    sumInteger(n);
+
+    myObj.close();
+}
+
+public static void sumInteger(int n) {
+   
+    int[] arrayInteger = new int [n];
+    int sum = 0;
+
+    for (int i = 0; i<=n; i++) {
+        sum += i;
+    }
+
+    System.out.println("The sum of the n valors is equal to: " +sum+ " .");
+}
 
 }
