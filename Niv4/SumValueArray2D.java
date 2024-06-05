400b8da9bfc9c8fd3351c546880b7054f5d6cbb6
bu-lo <161450788+bu-lo@users.noreply.github.com>
2024-06-05 15:28:44 +0200

    ex6.1 done

---
+++ Niv4/SumValueArray2D.java
@@ -0,0 +1,34 @@
+/*
+* Exercise 6.1 level 1 - "Manipulation of 2D/two-dimensional arrays."
+* Sum valors 2D array
+ */
+
+public class SumValueArray2D {
+
+    public static void main(String[] args) {
+        int[][] array2D = {
+            {4, 6, 5},
+            {7, 11, 34},
+            {28, 6, 2},
+            {1, 2, 3},
+            {4, 3, 10}
+        };
+        int height = 5;
+        int width = 3;
+
+        sumValueArray2D(array2D, width, height);
+    
+    }
+
+    public static void sumValueArray2D(int[][] array2D, int width, int height) {
+        int result = 0;
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                result += array2D[i][j];
+            }
+        }
+
+        System.out.println("The result/total is equal to: " + result + " .");
+    }
+}
