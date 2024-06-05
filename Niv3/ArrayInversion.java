9152fb2c7394d3163e746454792bbc844096ff17
bu-lo <161450788+bu-lo@users.noreply.github.com>
2024-06-05 12:33:51 +0200

    Ex 5.8 level 1 done

---
+++ Niv3/ArrayInversion.java
@@ -0,0 +1,31 @@
+/*
+ * Exercise 5.8 level 1 - "Manipulation of 1D/one-dimensional arrays."
+ * Invertion of values in an array
+ */
+
+
+import java.util.Arrays; // to use "Arrays.toString()""
+
+public class ArrayInversion {
+
+    public static void main(String[] args){
+        int[] array = {54, 1, 4, 6, 8};
+        
+        int[] newArray = invert(array);
+        System.out.println("The reversed array is: " + Arrays.toString(newArray));
+    }
+    
+
+    public static int[] invert(int[] array){
+        int[] newArray = new int[array.length]; // creation of a new tab, same length
+        
+        for (int i = 0; i<array.length; i++){
+            newArray [array.length -1 -i] = array [i];
+        }
+        
+        System.out.println("The first array is: " + Arrays.toString(array) + " .");
+        
+        return(newArray);
+    }
+}
+
